7d067004a8c7ea6f222211a54b3bdb02ac79a42e
Alex Rattray <rattray.alex@gmail.com>
2013-09-07 15:39:44 -0400

    first migration, hello world index.

--- app/views.py
+++ app/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+
+def index(request):
+  print 'hi'
+  return HttpResponse('Hello World')
